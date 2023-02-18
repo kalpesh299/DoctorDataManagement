62db805d0ba3ed57826325d0a5177ccbbc16d2d6
kalpesh patil <kalpesh123.kp78@gmail.com>
2023-02-19 02:55:58 +0530

    We have created petient model
    We have created petient model also created created the foregion key which mapping the doctor tabel foregign key is a id of doctors and also join pentents table with doctors tabel

--- src/main/java/com/example/DoctorDataManagement/dao/DoctorRepository.java
+++ src/main/java/com/example/DoctorDataManagement/dao/DoctorRepository.java
@@ -4,6 +4,6 @@ import com.example.DoctorDataManagement.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DoctorRepository extends JpaRepository<Doctor,Integer> {
+public interface DoctorRepository extends JpaRepository<Doctor,Integer> {//lassname,primarykey datatype
 
 }
---
+++ src/main/java/com/example/DoctorDataManagement/model/Petient.java
@@ -0,0 +1,38 @@
+package com.example.DoctorDataManagement.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="tbl_petient")
+public class Petient {
+
+    @Id
+    @Column(name="petient_id")
+    private String petientId;
+
+    @Column(name="petient_name")
+    private String petientName;
+    @Column(name="petient_age")
+    private Integer age;
+
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    @Column(name="disese_type")
+    private String diseseType;
+
+    @Column(name="petient_gender")
+    private String gender;
+
+@Column(name="admit_date")
+private Timestamp admitDate;
+    @JoinColumn(name="doctor_id")
+    @ManyToOne
+    private Doctor doctorId;//we always write the class name thats why i have use Doctor here
+
+
+
+}
